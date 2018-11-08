a4cde12492d74e708da3cf2b45bc6c5e92b6a38d
yaoshifeng <1320701765@qq.com>
2018-11-08 10:38:07 +0800

    first commit

---
+++ api/response.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	//	"github.com/"
+)
+
+//给客户端返回对应错误code
+func sendErrorResponse(w http.ResponseWriter) {
+
+}
+
+func sendNormalResponse(w http.ResponseWriter) {
+
+}
