b56ab9c8eaec36d932c17d4b8baa92abe13e6c84
yaoshifeng <1320701765@qq.com>
2018-11-09 15:58:46 +0800

    first commit

--- api/response.go
+++ api/response.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 
-	//	"github.com/"
+	"github.com/yaoshifeng/gowebvideoserver/api/def"
 )
 
 //给客户端返回对应错误code
-func sendErrorResponse(w http.ResponseWriter) {
-
+func sendErrorResponse(w http.ResponseWriter, errRep def.ErrorReponse) {
+	w.WriteHeader(errRep.HttpSC)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	status, _ := json.Marshal(&errRep.Error)
+	io.WriteString(w, string(status))
 }
 
-func sendNormalResponse(w http.ResponseWriter) {
-
+func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
 }
